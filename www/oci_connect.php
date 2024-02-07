37ffc7cdaf712df1c8b3341441a9852e7e63fc66
klek <klek@klek-dit.makc.ru>
2024-02-07 08:48:20 +0300

    Add all

---
+++ www/oci_connect.php
@@ -0,0 +1,22 @@
+<?php
+
+$conn = oci_connect('unicus_maks', 'maks', '//172.20.0.47:1521/MAKC', 'AL32UTF8');
+if (!$conn) {
+    $e = oci_error();
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+
+$stid = oci_parse($conn, 'SELECT * FROM users');
+oci_execute($stid);
+
+echo "<table border='1'>\n";
+while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+    echo "<tr>\n";
+    foreach ($row as $item) {
+        echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "") . "</td>\n";
+    }
+    echo "</tr>\n";
+}
+echo "</table>\n";
+
+?>
---
+++ www/phpinfo.php
@@ -0,0 +1,10 @@
+<?php
+//echo '<script type="text/javascript">  var SNOW_Picture = "https://uguide.ru/js/script/img_sneg.png"; // ссылка на изображение снежинки  var SNOW_Width = "55px"; // ширина изображения снежинки  var SNOW_Height = "55px"; // высота изображения снежинки  var SNOW_no = 99; // максимальное количество снежинок на странице одновременно</script><script type="text/javascript" src="https://uguide.ru/js/script/skript_sneg_webanfarwol_ru.js"></script>';
+
+//echo '<div style="position:absolute;top:0px;left:0px;width:300px;height:229px;background:url("https://uguide.ru/js/script/vetka.png")"></div>';
+echo '<script src="https://uguide.ru/js/script/snow11.js"> </script>';
+echo '<script type="text/javascript" src="https://uguide.ru/js/script/snowfall.min.js"></script>';
+echo '<div style="height: 30px; background: url(https://uguide.ru/js/script/girlianda_uguide_ru_1.gif) repeat-x 100%;"></div>';
+echo '<div align="center"><img alt="" src="https://uguide.ru/js/script/elka_ot_uguide_ru_2.gif" width="118" height="173"></div>';
+phpinfo();
+?>
